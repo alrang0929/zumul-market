3101acab9be6e8f65f25052a05cf8ffc858560fe
alrang0929 <159221214+alrang0929@users.noreply.github.com>
2024-12-23 04:35:06 +0900

    feature: 구글로그인 연결 완료

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/login.scss';
 import { BasicBtn } from '../../styles/Button';
+import GoogleLoginButton from '../../api/GoogleLoginBtn';
 const Login = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const Login = () => {
           <span className="title">
             당신의 작품을 지금 쪼물마켓에서<br/>세상과 연결하세요
           </span>
+          <GoogleLoginButton/>
           <BasicBtn className='login-btn'>로그인 하기</BasicBtn>
         </div>
       </div>
