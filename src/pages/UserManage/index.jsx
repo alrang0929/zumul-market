b1cf8351e85a7da6b234820da050d6d12cb7027f
sellbell07@gmail.com <sellbell07@gmail.com>
2025-01-23 19:08:53 +0900

    backup

--- src/pages/UserManage/index.jsx
+++ src/pages/UserManage/index.jsx
@@ -11,17 +11,12 @@ const COMPONENT_MAP = {
 };
 
 export const UserManagePage = () => {
-
   const user = useUserStore((state) => state.user);
   const Component = COMPONENT_MAP[user.type];
-  // type 이 없는 경우 error 처리
 
   return (
     <>
-      <div
-        className="user-manage-page"
-        style={{ backgroundColor: '#F5F5F5' }}
-      >
+      <div className="user-manage-page" style={{ backgroundColor: '#F5F5F5' }}>
         <UserInfoWrap />
         <DivBox>
           <Component />
