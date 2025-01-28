9be20bb4061d177fe4033efd8c702e959a725d72
sellbell07@gmail.com <sellbell07@gmail.com>
2025-01-28 18:31:08 +0900

    pref: swiper lazy loading finish

--- src/pages/Home/plugin/SwiperMainVisual.jsx
+++ src/pages/Home/plugin/SwiperMainVisual.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -11,12 +11,7 @@ import 'swiper/css/effect-coverflow';
 import './swiper_main_visual.scss';
 import { SliderData } from './db';
 // import required modules
-import {
-  Autoplay,
-  Pagination,
-  Navigation,
-  EffectFade,
-} from 'swiper/modules';
+import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper/modules';
 
 export default function MainVisualSlider() {
   return (
@@ -28,20 +23,14 @@ export default function MainVisualSlider() {
           delay: 2500,
           disableOnInteraction: true,
         }}
-        // pagination={{
-        //   clickable: true,
-        // }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation, EffectFade]}
         effect="fade"
+        // lazy={{ loadPrevNext: true }}
         className="main-visual-swiper"
       >
         {SliderData.map((item, i) => (
-          <SwiperSlide
-            className="inner-slide"
-            key={'slide' + i}
-            style={{ backgroundImage: `url(${item.bgURL})` }}
-          >
+          <SwiperSlide className="inner-slide" key={'slide' + i}>
             <div className="text-wrap">
               <div className="topic">{item.topic}</div>
               <div className="title-wrap">
@@ -49,6 +38,14 @@ export default function MainVisualSlider() {
                   <span key={i}>{split}</span>
                 ))}
               </div>
+            </div>
+            <div className="bg-box" >
+              <img 
+              src={item.bgURL} 
+              alt={item.title + '썸네일'} 
+              loading="lazy"
+              />
+              <div className="swiper-lazy-preloader"></div>
             </div>
           </SwiperSlide>
         ))}
